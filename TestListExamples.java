0bcbf35b47caf114e836b3d981cd6304b0285afc
AvailableUsernameA <huc014@ucsd.edu>
2022-11-12 21:49:07 -0800

    a

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+    public void testMerge() {
+        List<String> input1 = new ArrayList<>();
+        input1.add("a");
+        input1.add("b");
+        List<String> input2 = new ArrayList<>();
+        input2.add("c");
+        List<String> result = ListExamples.merge(input1, input2);
+        List<String> expect = new ArrayList<>();
+        expect.add("a");
+        expect.add("b");
+        expect.add("c");
+        assertEquals(expect, result);
+    }
 }
